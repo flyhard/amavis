75a974ebe2377e48139d9443fcd21de98e59f83f
Per Abich <per.abich@gmail.com>
2016-04-30 20:45:02 +0100

    Amavis is runnable now. Not tested yet.

--- run.sh
+++ run.sh
@@ -7,11 +7,11 @@ else
 fi
 consul agent -config-dir /etc/consul -join ${consul_node} &
 
-sleep 15
+sleep 2
 
 curl -X PUT -d "$HOSTNAME.local" http://localhost:8500/v1/kv/mail/amavis/hostname
 
-sleep 5
+sleep 1
 
 /usr/local/bin/confd -onetime -backend consul -node consul:8500
 
